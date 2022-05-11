ea21153466636363cd006532536d50daafa6a9b7
radioation <greggallardo@gmail.com>
2022-05-10 23:48:38 -0500

    starting menus resolver

--- src/menus/menus.module.ts
+++ src/menus/menus.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MenusService } from './menus.service';
+import { MenusResolver } from './menus.resolver';
 
 @Module({
-  providers: [MenusService]
+  providers: [MenusService, MenusResolver]
 })
 export class MenusModule {}
