96eed9b07bd3e0949577e0e34c381d8704e491c3
radioation <greggallardo@gmail.com>
2022-05-10 23:57:44 -0500

    addin graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+
+// GraphQL 
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MenusModule } from './menus/menus.module';
 
 @Module({
-  imports: [MenusModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+    MenusModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
