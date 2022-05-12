62be1142077a2843445cfad809fe88411ef44c0e
radioation <greggallardo@gmail.com>
2022-05-11 23:20:46 -0500

    Adding dto and model files for Item

---
+++ src/items/models/item.model.ts
@@ -0,0 +1,24 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToMany } from 'typeorm';
+import { Directive, Field, ID, ObjectType } from '@nestjs/graphql';
+import { NumericTransformer } from '../../numeric.transformer';
+
+@ObjectType({ description: 'item' })
+@Entity()
+export class Item {
+  @Field()
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Field()
+  @Column({ length: 50, nullable: false })
+  name: string;
+
+  @Field({ nullable: true })
+  @Column()
+  description?: string;
+
+  @Field()
+  @Column({ name: 'price', type: 'decimal', precision: 10, scale: 2, default: 0.0, transformer: new NumericTransformer() })
+  price: string;
+
+}
